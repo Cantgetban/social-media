ae4d8d7fe5ade3f690244dd03274285f492432d4
Amit Cohen <85591227+Cantgetban@users.noreply.github.com>
2023-01-23 14:27:23 +0200

    Add files via upload

---
+++ build/precache-manifest.e34206bee84d1eec8dcad215952aeb3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a009139c35751e0030de",
+    "url": "/static/css/main.fa85efd1.chunk.css"
+  },
+  {
+    "revision": "a009139c35751e0030de",
+    "url": "/static/js/main.7f54b5e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e022c12a2c582918f1ac",
+    "url": "/static/js/2.38344a8f.chunk.js"
+  },
+  {
+    "revision": "e108048e035af13d5f1898d255aedd7e",
+    "url": "/index.html"
+  }
+];
